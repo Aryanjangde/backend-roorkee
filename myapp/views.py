e556dc86c9ecb8aa1b18e69c68fc04eecb38316d
Aryanjangde <145124628+Aryanjangde@users.noreply.github.com>
2024-07-26 13:42:27 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -9,7 +9,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
-from django.http import JsonResponse
+
 
 
 from .models import (
@@ -507,5 +507,4 @@ class CategoryChoicesView(APIView):
         return Response(CustomUser._meta.get_field('category').choices, status=status.HTTP_200_OK)
 
 
-def health_check(request):
-    return JsonResponse({'status': 'ok'})
+
